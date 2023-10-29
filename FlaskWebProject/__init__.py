9344250322c51d2c6f7004b6b56da838b9b23237
minhdua <vothanhhienag1996@gmail.com>
2023-10-30 01:04:00 +0700

    resole todo

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,20 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.INFO) 
+
+# Create a file handler to log to a file
+file_handler = logging.FileHandler('login.log')
+file_handler.setLevel(logging.INFO)  # Set the desired logging level for this handler
+file_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s'))
+app.logger.addHandler(file_handler)
+
+# Create a console handler to log to the console (optional)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)  # Set the desired logging level for this handler
+console_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s'))
+app.logger.addHandler(console_handler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
--- config.py
+++ config.py
@@ -1,29 +1,23 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-from decouple import Config, Csv
-
-config = Config()
-config.read_dotenv()
-
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+    SECRET_KEY = os.getenv('SECRET_KEY', default='minhdua@1234567890')
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.getenv('BLOB_ACCOUNT', default='minhduablob')
+    BLOB_STORAGE_KEY = os.getenv('BLOB_STORAGE_KEY', default='iJb6WpBAoC737vxlC27+cNFvCXQe9qf5zWxFJkn8sruv1YMbD0ZtpwoPaudgw35Jec2L1ddrKwp3+AStsbtP2A==')
+    BLOB_CONTAINER = os.getenv('BLOB_CONTAINER', default='images')
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.getenv('SQL_SERVER', default='minhdua-sql-server.database.windows.net')
+    SQL_DATABASE = os.getenv('SQL_DATABASE', default='minhdua-db')
+    SQL_USER_NAME = os.getenv('SQL_USER_NAME', default='mdadmin')
+    SQL_PASSWORD = os.getenv('SQL_PASSWORD', default='minhdua_12345')
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    print(SQLALCHEMY_DATABASE_URI)
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET', default='QN48Q~gPQpwOkXwlNaojdV1JZbOS-QZRjaESYbit')
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -31,10 +25,10 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    AUTHORITY = os.getenv('AUTHORITY', default='https://login.microsoftonline.com/common')  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = os.getenv('CLIENT_ID', default='c9969ba4-9dd5-45cd-b0e6-8b58e6f9cb78')
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
